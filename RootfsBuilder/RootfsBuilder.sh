298124ccad96d52743a5b793ecf385547bb30360
xinli <xinligg@gmail.com>
2019-11-05 09:00:53 +0800

    在pkginstall后添加replacefile操作

--- RootfsBuilder/RootfsBuilder.sh
+++ RootfsBuilder/RootfsBuilder.sh
@@ -1615,6 +1615,7 @@ doMain()
                 doInstallExtraPackages || exit $?
                 SetUserPassword ${RootDir} ${AccountUsername} ${AccountPassword} || exit $?
                 SetupBootloader ${VDisk} ${RootDir} ${BootloaderID} || exit $?
+                ReplaceFiles ${RootDir} ${ProfilesDir} ${ReplaceFiles} || exit $?
                 UnLoadVirtualDisk ${VDisk} || exit $?
                 ;;
             -z|z|zip)
